75676eb44e26648ba48249b56cd9fae0d9337680
Steve Doty <stevedoty16@gmail.com>
2020-08-03 06:41:38 -0700

    add docs folder to build

---
+++ docs/precache-manifest.0b3363c5869c22fa91836f4317b73586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067e2b179c4882ba541733ecf291c871",
+    "url": "/stock-app/index.html"
+  },
+  {
+    "revision": "c8bcc7a8de7b012c1c77",
+    "url": "/stock-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87eb8f61b11001de9db7",
+    "url": "/stock-app/static/js/2.9a8d7a09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-app/static/js/2.9a8d7a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8bcc7a8de7b012c1c77",
+    "url": "/stock-app/static/js/main.5042808d.chunk.js"
+  },
+  {
+    "revision": "137b78523794659ad0ef",
+    "url": "/stock-app/static/js/runtime-main.a5f7fdd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stock-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
